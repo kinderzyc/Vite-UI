e1ff4f692ddae7f9ca2d1898f8bf5b0f587ea2ac
Your Namekinderzyc <75829215@qq.com>
2020-09-16 17:06:39 +0800

    frist  commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />-->
+  <Top />
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld.vue";
+import Top from "./components/Top.vue";
+
+export default {
+  name: "App",
+  components: {
+    Top
+  }
+};
+</script>
---
+++ src/components/Top.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>我是一个组件</div>
+</template>
