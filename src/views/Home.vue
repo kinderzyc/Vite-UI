92e09a8b98ce2fa19cb150c4a77e95789a87dcb1
Your Namekinderzyc <75829215@qq.com>
2020-09-21 11:23:24 +0800

    aside样式初始化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,8 @@
       <h1>轱辘UI</h1>
       <h2>一个Vue3 UI框架</h2>
       <p class="actions">
-        <a href>GitHub</a>
-        <a href>开始</a>
+        <a href="https://github.com/kinderzyc/Vite-UI">GitHub</a>
+       <router-link to="/doc">开始</router-link>
       </p>
     </div>
   </div>
