7c9b433f2e8ce226caba716d90b23685831f3251
Your Namekinderzyc <75829215@qq.com>
2020-09-30 21:05:13 +0800

    Dialog支持点击关闭

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,13 +1,27 @@
 <template>
   <div>Dialog示例</div>
   <h1>示例</h1>
-  <Dialog></Dialog>
+  <Button @click="toggle">toggle</Button>
+  <Dialog v-model:visiable="x" :closeOnClickOverlay="false" :ok="f1" :cancel="f2"></Dialog>
 </template>
 
 <script lang="ts">
 import Dialog from "../Lib/Dialog.vue";
+import Button from "../Lib/Button.vue";
+import { ref } from "vue";
 export default {
-  components: { Dialog }
+  components: { Dialog, Button },
+  setup() {
+    const x = ref(false);
+    const toggle = () => {
+      x.value = !x.value;
+    };
+    const f1 = () => {
+      return false;
+    };
+    const f2 = () => {};
+    return { x, toggle, f1, f2 };
+  }
 };
 </script>
 
