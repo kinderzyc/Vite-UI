d8606453871196a37516670aa08a6d2b00724b14
Your Namekinderzyc <75829215@qq.com>
2020-09-30 13:36:30 +0800

    创建Dialog组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,15 +1,15 @@
 <template>
-    <div>
-        DialogDemo
-    </div>
+  <div>Dialog示例</div>
+  <h1>示例</h1>
+  <Dialog></Dialog>
 </template>
 
 <script lang="ts">
-    export default {
-        
-    }
+import Dialog from "../Lib/Dialog.vue";
+export default {
+  components: { Dialog }
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
