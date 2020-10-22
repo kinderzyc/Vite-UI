68c3aac7dc4b02669efaf13894aca594e2a2771d
Your Namekinderzyc <75829215@qq.com>
2020-10-22 12:19:30 +0800

    引入Github样式

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import './Lib/gulu.scss'
 import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './router'
+import 'github-markdown-css'
 
 
 const app = createApp(App)
